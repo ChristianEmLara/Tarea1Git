f00733814bef7532f8ec64dc45d48fa78fe8503c
Christian Lara <christian.dm@live.com>
2016-10-01 13:24:00 +0200

    Primera version

---
+++ archivo_git.py
@@ -0,0 +1,7 @@
+
+class Vuelo(): #Otro 
+    def __init__(self, origen, destino, distancia):#constructor
+        self.origen = origen  
+        self.destino = destino
+        self.distancia = distancia
+
